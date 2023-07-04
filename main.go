c260ded45a3118f760a7e3c13724a2dcc3a3f36f
r3drun3 <simone.ragonesi@sighup.io>
2023-07-04 11:28:29 +0200

    feat: factor api calls into single function

--- main.go
+++ main.go
@@ -33,18 +33,14 @@ func main() {
 
 	fmt.Println("\n[Files present in", folder1, "but not in", folder2, "====>]")
 	// Compare folders and get files present in folder1 but not in folder2
-	diffFiles, err := compareFolders(client, repoURL, folder1, folder2)
+	diffFiles, newerFiles, err := compareFolders(client, repoURL, folder1, folder2)
 	if err != nil {
 		log.Fatal(err)
 	}
 	printFilesSorted(diffFiles)
 
 	fmt.Println("\n\n[Files present in both", folder1, "and", folder2, "with newer commits in", folder1, "====>]")
-	// Get files present in both folder1 and folder2 with newer commits in folder1
-	newerFiles, err := getFilesWithNewerCommit(client, repoURL, folder1, folder2)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// Print files present in both folder1 and folder2 with newer commits in folder1
 	printFilesSorted(newerFiles)
 }
 
@@ -75,23 +71,23 @@ func createGitHubClient(token string) *github.Client {
 	return github.NewClient(tc)
 }
 
-// Compare folders and get files present in folder1 but not in folder2
-func compareFolders(client *github.Client, repoURL, folder1, folder2 string) ([]*github.RepositoryContent, error) {
+// Compare folders and get files present in folder1 but not in folder2, and files with newer commits in folder1
+func compareFolders(client *github.Client, repoURL, folder1, folder2 string) ([]*github.RepositoryContent, []*github.RepositoryContent, error) {
 	owner, repo := parseRepoURL(repoURL)
 
 	// Get contents of folder1 and folder2 from the GitHub repository
-	_, folder1Files, _, err := client.Repositories.GetContents(context.Background(), owner, repo, folder1, nil)
+	folder1Files, err := getFolderContents(client, owner, repo, folder1)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
-	_, folder2Files, _, err := client.Repositories.GetContents(context.Background(), owner, repo, folder2, nil)
+	folder2Files, err := getFolderContents(client, owner, repo, folder2)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	diffFiles := make([]*github.RepositoryContent, 0)
-	diffChan := make(chan *github.RepositoryContent)
+	newerFiles := make([]*github.RepositoryContent, 0)
 
 	var wg sync.WaitGroup
 	wg.Add(len(folder1Files))
@@ -109,84 +105,37 @@ func compareFolders(client *github.Client, repoURL, folder1, folder2 string) ([]
 				}
 			}
 			if !found {
-				diffChan <- file
-			}
-		}(file1)
-	}
-
-	// Wait for all goroutines to finish and close the diffChan channel
-	go func() {
-		wg.Wait()
-		close(diffChan)
-	}()
-
-	// Collect the different files from the diffChan channel
-	for file := range diffChan {
-		diffFiles = append(diffFiles, file)
-	}
-
-	return diffFiles, nil
-}
-
-// Get files present in both folder1 and folder2 with newer commits in folder1
-func getFilesWithNewerCommit(client *github.Client, repoURL, folder1, folder2 string) ([]*github.RepositoryContent, error) {
-	owner, repo := parseRepoURL(repoURL)
-
-	// Get contents of folder1 and folder2 from the GitHub repository
-	_, folder1Files, _, err := client.Repositories.GetContents(context.Background(), owner, repo, folder1, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	_, folder2Files, _, err := client.Repositories.GetContents(context.Background(), owner, repo, folder2, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	newerFiles := make([]*github.RepositoryContent, 0)
-	newerChan := make(chan *github.RepositoryContent)
-
-	var wg sync.WaitGroup
-	wg.Add(len(folder1Files))
-
-	// Compare files in folder1 with files in folder2 and check for newer commits in folder1 using goroutines
-	for _, file1 := range folder1Files {
-		go func(file *github.RepositoryContent) {
-			defer wg.Done()
-
-			for _, file2 := range folder2Files {
-				if *file.Name == *file2.Name {
-					commit1, err := getFileLastCommit(client, owner, repo, folder1, *file.Name)
-					if err != nil {
-						log.Println(err)
-						return
-					}
-					commit2, err := getFileLastCommit(client, owner, repo, folder2, *file2.Name)
-					if err != nil {
-						log.Println(err)
-						return
-					}
-					if commit1 != nil && commit2 != nil && commit1.GetCommit().GetCommitter().GetDate().Time.After(commit2.GetCommit().GetCommitter().GetDate().Time) {
-						newerChan <- file
-					}
-					break
+				diffFiles = append(diffFiles, file)
+			} else {
+				commit1, err := getFileLastCommit(client, owner, repo, folder1, *file.Name)
+				if err != nil {
+					log.Println(err)
+					return
+				}
+				commit2, err := getFileLastCommit(client, owner, repo, folder2, *file.Name)
+				if err != nil {
+					log.Println(err)
+					return
+				}
+				if commit1 != nil && commit2 != nil && commit1.GetCommit().GetCommitter().GetDate().Time.After(commit2.GetCommit().GetCommitter().GetDate().Time) {
+					newerFiles = append(newerFiles, file)
 				}
 			}
 		}(file1)
 	}
 
-	// Wait for all goroutines to finish and close the newerChan channel
-	go func() {
-		wg.Wait()
-		close(newerChan)
-	}()
+	wg.Wait()
 
-	// Collect the files with newer commits from the newerChan channel
-	for file := range newerChan {
-		newerFiles = append(newerFiles, file)
+	return diffFiles, newerFiles, nil
+}
+
+// Get contents of a folder from the GitHub repository
+func getFolderContents(client *github.Client, owner, repo, folder string) ([]*github.RepositoryContent, error) {
+	_, files, _, err := client.Repositories.GetContents(context.Background(), owner, repo, folder, nil)
+	if err != nil {
+		return nil, err
 	}
-
-	return newerFiles, nil
+	return files, nil
 }
 
 // Get the last commit of a file in a specific path
